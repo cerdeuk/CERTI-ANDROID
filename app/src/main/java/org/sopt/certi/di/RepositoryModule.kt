2f750a1bf0d54f421168506de38fb2578515c084
MinseoSONG <mins041600@naver.com>
2025-06-29 02:03:05 +0900

    [init] RepositoryModule and DummyRepository 바인딩

---
+++ app/src/main/java/org/sopt/certi/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package org.sopt.certi.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+import org.sopt.certi.data.repositoryimpl.DummyRepositoryImpl
+import org.sopt.certi.domain.repository.DummyRepository
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindDummyRepository(dummyRepositoryImpl: DummyRepositoryImpl): DummyRepository
+}
