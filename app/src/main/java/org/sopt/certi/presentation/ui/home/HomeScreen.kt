426cc2ceea3efb593ef65e2d09ea268d4340a3cd
MinseoSONG <mins041600@naver.com>
2025-06-29 02:23:20 +0900

    [init] 예시용 HomeScreen 추가

--- app/src/main/java/org/sopt/certi/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/home/HomeScreen.kt
@@ -1,2 +1,30 @@
 package org.sopt.certi.presentation.ui.home
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import org.sopt.certi.ui.theme.CERTITheme
+
+@Composable
+fun HomeRoute(
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    HomeScreen()
+}
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewHomeScreen() {
+    CERTITheme {
+        HomeScreen()
+    }
+}
