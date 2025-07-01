a34966ce6b1078071c1c93f5386cdb0ae808c1fd
MinseoSONG <mins041600@naver.com>
2025-07-01 22:26:54 +0900

    [feat] 스플래시 구현

---
+++ app/src/main/java/org/sopt/certi/presentation/ui/splash/SplashViewModel.kt
@@ -0,0 +1,23 @@
+package org.sopt.certi.presentation.ui.splash
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor() : ViewModel() {
+    private val _navigateToLogin = MutableStateFlow(false)
+    val navigateToLogin = _navigateToLogin.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(2000)
+            _navigateToLogin.value = true
+        }
+    }
+}
