d4d2ae5b3f79b3f63b34d080661abdc60810642b
mjth1s1s <navygrace8389@gmail.com>
2025-07-04 18:33:18 +0900

    [add] TypographyTokens 정의

--- app/src/main/java/org/sopt/certi/ui/theme/Type.kt
+++ app/src/main/java/org/sopt/certi/ui/theme/Type.kt
@@ -1,34 +1,52 @@
 package org.sopt.certi.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.sopt.certi.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+
+object PretendardFont {
+    val Bold = FontFamily(Font(R.font.pretendard_bold))
+    val SemiBold = FontFamily(Font(R.font.pretendard_semibold))
+    val Regular = FontFamily(Font(R.font.pretendard_regular))
+}
+
+sealed interface TypographyTokens {
+
+    @Immutable
+    data class Title(
+        val bold_24: TextStyle,
+        val bold_22: TextStyle
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+    @Immutable
+    data class Subtitle(
+        val bold_20: TextStyle,
+        val semibold_20: TextStyle
     )
-    */
-)
+
+    @Immutable
+    data class Body(
+        val bold_18: TextStyle,
+        val semibold_18: TextStyle,
+        val bold_16: TextStyle,
+        val semibold_16: TextStyle,
+        val regular_16: TextStyle
+    )
+
+    @Immutable
+    data class Caption(
+        val bold_14: TextStyle,
+        val semibold_14: TextStyle,
+        val regular_14: TextStyle,
+        val semibold_12: TextStyle,
+        val regular_12: TextStyle,
+        val semibold_10: TextStyle,
+        val regular_10: TextStyle
+    )
+}
