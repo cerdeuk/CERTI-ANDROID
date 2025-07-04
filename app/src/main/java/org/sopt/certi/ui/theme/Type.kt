e57f1612fae5fb2054ebf472c50272dd9575bfa1
mjth1s1s <navygrace8389@gmail.com>
2025-07-04 18:39:34 +0900

    [add] CertiTypography 정의

--- app/src/main/java/org/sopt/certi/ui/theme/Type.kt
+++ app/src/main/java/org/sopt/certi/ui/theme/Type.kt
@@ -1,5 +1,6 @@
 package org.sopt.certi.ui.theme
 
+import android.R.attr.fontFamily
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.TextStyle
@@ -50,3 +51,39 @@ sealed interface TypographyTokens {
         val regular_10: TextStyle
     )
 }
+
+@Immutable
+data class CertiTypography(
+    val title: TypographyTokens.Title,
+    val subtitle: TypographyTokens.Subtitle,
+    val body: TypographyTokens.Body,
+    val caption: TypographyTokens.Caption
+)
+
+val defaultCertiTypography = CertiTypography(
+    title = TypographyTokens.Title(
+        bold_24 = TextStyle(fontFamily = PretendardFont.Bold, fontSize = 24.sp),
+        bold_22 = TextStyle(fontFamily = PretendardFont.Bold, fontSize = 22.sp)
+    ),
+    subtitle = TypographyTokens.Subtitle(
+        bold_20 = TextStyle(fontFamily = PretendardFont.Bold, fontSize = 20.sp),
+        semibold_20 = TextStyle(fontFamily = PretendardFont.SemiBold, fontSize = 20.sp)
+    ),
+    body = TypographyTokens.Body(
+        bold_18 = TextStyle(fontFamily = PretendardFont.Bold, fontSize = 18.sp),
+        semibold_18 = TextStyle(fontFamily = PretendardFont.SemiBold, fontSize = 18.sp),
+        bold_16 = TextStyle(fontFamily = PretendardFont.Bold, fontSize = 16.sp),
+        semibold_16 = TextStyle(fontFamily = PretendardFont.SemiBold, fontSize = 16.sp),
+        regular_16 = TextStyle(fontFamily = PretendardFont.Regular, fontSize = 16.sp)
+    ),
+    caption = TypographyTokens.Caption(
+        bold_14 = TextStyle(fontFamily = PretendardFont.Bold, fontSize = 14.sp),
+        semibold_14 = TextStyle(fontFamily = PretendardFont.SemiBold, fontSize = 14.sp),
+        regular_14 = TextStyle(fontFamily = PretendardFont.Regular, fontSize = 14.sp),
+        semibold_12 = TextStyle(fontFamily = PretendardFont.SemiBold, fontSize = 12.sp),
+        regular_12 = TextStyle(fontFamily = PretendardFont.Regular, fontSize = 12.sp),
+        semibold_10 = TextStyle(fontFamily = PretendardFont.SemiBold, fontSize = 10.sp),
+        regular_10 = TextStyle(fontFamily = PretendardFont.Regular, fontSize = 10.sp)
+    )
+)
+
