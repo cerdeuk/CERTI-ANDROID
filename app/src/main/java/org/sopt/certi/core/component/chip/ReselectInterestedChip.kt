57f16da0a0f3bff4f350342cd60505c69e384b8f
kimjw2003 <kimjw111003@gmail.com>
2025-07-07 23:25:59 +0900

    [feat] 희망분야 재설정 칩 제작

---
+++ app/src/main/java/org/sopt/certi/core/component/chip/ReselectInterestedChip.kt
@@ -0,0 +1,59 @@
+package org.sopt.certi.core.component.chip
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.certi.R
+import org.sopt.certi.core.util.pressedClickable
+import org.sopt.certi.ui.theme.CertiTheme
+
+@Composable
+fun ReselectInterestedChip(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    var isPressed by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier
+            .background(color = if (isPressed) CertiTheme.colors.lightBlue else CertiTheme.colors.white, shape = RoundedCornerShape(24.dp))
+            .clip(RoundedCornerShape(24.dp))
+            .border(width = 1.dp, color = CertiTheme.colors.mainBlue, shape = RoundedCornerShape(24.dp))
+            .pressedClickable(
+                changePressed = {
+                    isPressed = it
+                },
+                onClick = {
+                    onClick()
+                }
+            )
+    ) {
+        Text(
+            text = stringResource(R.string.reselect_interested_chip_text),
+            style = CertiTheme.typography.caption.semibold_12,
+            color = CertiTheme.colors.mainBlue,
+            modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ReselectInterestedChipPreview() {
+    ReselectInterestedChip(
+        onClick = {}
+    )
+}
