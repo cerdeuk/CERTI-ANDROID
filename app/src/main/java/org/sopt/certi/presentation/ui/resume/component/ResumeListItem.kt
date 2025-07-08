6de5f9eb37550308509a1e067d8bce4083ff9ea1
Jyunee54 <jyunee54@gmail.com>
2025-07-08 15:42:13 +0900

    [feat] 이미지 사이즈 수정

--- app/src/main/java/org/sopt/certi/presentation/ui/resume/component/ResumeListItem.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/resume/component/ResumeListItem.kt
@@ -4,14 +4,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,13 +28,17 @@ fun ResumeListItem(
     description: String
 ) {
     Row(
-        modifier = Modifier.padding(vertical = 12.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
             painter = painterResource(R.drawable.img_resume_list_item),
             contentDescription = null,
-            modifier = Modifier.size(24.dp)
+            modifier = Modifier
+                .width((LocalConfiguration.current.screenWidthDp * 0.1f).dp)
+                .height((LocalConfiguration.current.screenHeightDp * 0.05f).dp)
         )
         Spacer(modifier = Modifier.width(24.dp))
 
