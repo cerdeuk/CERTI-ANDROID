7fb4761b0a4f6062e39e11c562f28ce2d721dfc1
Jyunee54 <jyunee54@gmail.com>
2025-07-08 15:18:25 +0900

    [chore] 기기대응 수정

--- app/src/main/java/org/sopt/certi/core/component/topbar/CertiTopBar.kt
+++ app/src/main/java/org/sopt/certi/core/component/topbar/CertiTopBar.kt
@@ -3,10 +3,12 @@ package org.sopt.certi.core.component.topbar
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,7 +26,10 @@ fun CertiTopBar() {
         Image(
             painter = painterResource(R.drawable.img_logo_black),
             contentDescription = null,
-            modifier = Modifier.width(75.dp)
+            modifier = Modifier
+                .height((LocalConfiguration.current.screenHeightDp * 0.04f).dp)
+                .width((LocalConfiguration.current.screenWidthDp * 0.23f).dp)
+
         )
     }
 }
