ea82bf39c6ada818e93e65140cca6ee1f614ffed
kimjw2003 <kimjw111003@gmail.com>
2025-07-08 23:19:01 +0900

    [feat] 추천 필터 Selectable 버튼 구현

---
+++ app/src/main/java/org/sopt/certi/presentation/ui/certrecommend/component/button/RecommendFilterSelectableButton.kt
@@ -0,0 +1,68 @@
+package org.sopt.certi.presentation.ui.certrecommend.component.button
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.sopt.certi.core.util.pressedClickable
+import org.sopt.certi.ui.theme.CertiTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.certi.core.util.noRippleClickable
+import org.sopt.certi.domain.type.CategoryType
+
+@Composable
+fun RecommendFilterSelectableButton(
+    categoryType: CategoryType,
+    onClick: (CategoryType) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var isSelected by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier
+            .width(100.dp)
+            .height(46.dp)
+            .background(
+                color = if(isSelected) CertiTheme.colors.lightBlue else CertiTheme.colors.white,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .clip(RoundedCornerShape(12.dp))
+            .border(
+                width = 1.dp,
+                color = if (isSelected) CertiTheme.colors.skyBlue else CertiTheme.colors.lightBlue,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .noRippleClickable {
+                isSelected = !isSelected
+                onClick(categoryType)
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = categoryType.description,
+            style = CertiTheme.typography.caption.semibold_12,
+            color = CertiTheme.colors.purpleBlue,
+            modifier = Modifier.padding(vertical = 12.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun RecommendFilterSelectableButtonPreview() {
+    RecommendFilterSelectableButton(categoryType = CategoryType.MARKETING, onClick = {})
+}
