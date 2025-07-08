5f7c1ffcb8c6c86c05b256d57b8c8a42f824e927
kimjw2003 <kimjw111003@gmail.com>
2025-07-09 00:58:07 +0900

    [chore] 코드리뷰 적용

--- app/src/main/java/org/sopt/certi/presentation/ui/certrecommend/component/button/RecommendFilterSelectableButton.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/certrecommend/component/button/RecommendFilterSelectableButton.kt
@@ -1,11 +1,7 @@
 package org.sopt.certi.presentation.ui.certrecommend.component.button
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -13,36 +9,30 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import org.sopt.certi.ui.theme.CertiTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import org.sopt.certi.core.util.noRippleClickable
+import org.sopt.certi.core.util.roundedBackgroundWithBorder
 import org.sopt.certi.domain.type.CategoryType
 
 @Composable
 fun RecommendFilterSelectableButton(
     categoryType: CategoryType,
+    isSelected: Boolean,
     onClick: (CategoryType) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var isSelected by remember { mutableStateOf(false) }
-
     Box(
         modifier = modifier
-            .background(
-                color = if (isSelected) CertiTheme.colors.lightBlue else CertiTheme.colors.white,
-                shape = RoundedCornerShape(12.dp)
-            )
-            .clip(RoundedCornerShape(12.dp))
-            .border(
-                width = 1.dp,
-                color = if (isSelected) CertiTheme.colors.skyBlue else CertiTheme.colors.lightPurple,
-                shape = RoundedCornerShape(12.dp)
+            .roundedBackgroundWithBorder(
+                cornerRadius = 12.dp,
+                backgroundColor = if (isSelected) CertiTheme.colors.lightBlue else CertiTheme.colors.white,
+                borderColor = if (isSelected) CertiTheme.colors.skyBlue else CertiTheme.colors.lightPurple,
+                borderWidth = 1.dp
             )
             .noRippleClickable {
-                isSelected = !isSelected
                 onClick(categoryType)
             },
         contentAlignment = Alignment.Center
@@ -56,11 +46,16 @@ fun RecommendFilterSelectableButton(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun RecommendFilterSelectableButtonPreview() {
+    var isSelected by remember { mutableStateOf(false) }
+
     RecommendFilterSelectableButton(
         categoryType = CategoryType.MARKETING,
-        onClick = {}
+        isSelected = isSelected,
+        onClick = {
+            isSelected = !isSelected
+        }
     )
 }
