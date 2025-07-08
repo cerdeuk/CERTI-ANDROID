799f9764ade282ee8a8caa31b657d9cf8853c6a8
MinseoSONG <mins041600@naver.com>
2025-07-08 20:41:36 +0900

    [feat] 계열 enum class 생성(track)

---
+++ app/src/main/java/org/sopt/certi/domain/type/TrackType.kt
@@ -0,0 +1,27 @@
+package org.sopt.certi.domain.type
+
+enum class TrackType(
+    val description: String
+) {
+    HUMANITIES(
+        description = "인문계열"
+    ),
+    SOCIAL(
+        description = "사회계열"
+    ),
+    EDUCATION(
+        description = "교육계열"
+    ),
+    NATURAL(
+        description = "자연계열"
+    ),
+    ENGINEERING(
+        description = "공학계열"
+    ),
+    MEDICAL(
+        description = "의약계열"
+    ),
+    ARTS(
+        description = "예체능계열"
+    ),
+}
