33c4571fa8cade89734f7a4639edc8d28f5e6279
MinseoSONG <mins041600@naver.com>
2025-07-08 16:55:27 +0900

    [chore] lint 적용

--- app/src/main/java/org/sopt/certi/presentation/ui/main/MainBottomBar.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/main/MainBottomBar.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.semantics.Role
--- app/src/main/java/org/sopt/certi/presentation/ui/splash/SplashScreen.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/splash/SplashScreen.kt
@@ -5,16 +5,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
