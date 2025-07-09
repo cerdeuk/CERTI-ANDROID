a7722cb4620ab7268a064dadeeb56fd87ae054d3
Jyunee54 <jyunee54@gmail.com>
2025-07-09 18:38:11 +0900

    [fix] 패딩 수정

--- app/src/main/java/org/sopt/certi/presentation/ui/resume/component/ResumeCertificationSmallCard.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/resume/component/ResumeCertificationSmallCard.kt
@@ -3,13 +3,11 @@ package org.sopt.certi.presentation.ui.resume.component
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -22,6 +20,8 @@ import coil.request.ImageRequest
 import org.sopt.certi.R
 import org.sopt.certi.core.component.chip.CertiChipList
 import org.sopt.certi.core.util.heightForScreenPercentage
+import org.sopt.certi.core.util.screenHeightDp
+import org.sopt.certi.core.util.screenWidthDp
 import org.sopt.certi.core.util.widthForScreenPercentage
 import org.sopt.certi.ui.theme.CERTITheme
 import org.sopt.certi.ui.theme.CertiTheme
@@ -52,21 +52,21 @@ fun ResumeCertificationSmallCard(
 
         Column(
             modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 32.dp)
+                .padding(horizontal = screenWidthDp(0.04f), vertical = screenHeightDp(0.04f))
         ) {
             Text(
                 text = name,
                 style = CertiTheme.typography.caption.bold_14,
                 color = CertiTheme.colors.gray600
             )
-            Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(screenHeightDp(0.005f)))
 
             Text(
                 text = stringResource(id = R.string.resume_certification_card_small, year, month, day),
                 style = CertiTheme.typography.caption.regular_12,
                 color = CertiTheme.colors.gray600
             )
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(screenHeightDp(0.01f)))
 
             CertiChipList(categories = tags)
         }
@@ -77,18 +77,13 @@ fun ResumeCertificationSmallCard(
 @Composable
 fun ResumeCertificationSmallCardPreview() {
     CERTITheme {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            ResumeCertificationSmallCard(
-                name = "GTQ 1급 (그래픽기술자격)",
-                year = 2025,
-                month = 7,
-                day = 3,
-                cardImageUrl = "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTBfMTgx/MDAxNTc4NjM1MTAxNjk1.m2q2MOZR3vArhqg1nC4-i2CEaVPlcPNcbic3KyTGj-cg.BBprGk0SqCmOMngKaT1CaaR_IBTJ8t-4LrOu_Nn2prAg.JPEG.p197273/88aad6.jpg?type=w800",
-                tags = listOf("태그", "태그", "태그")
-            )
-        }
+        ResumeCertificationSmallCard(
+            name = "GTQ 1급 (그래픽기술자격)",
+            year = 2025,
+            month = 7,
+            day = 3,
+            cardImageUrl = "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTBfMTgx/MDAxNTc4NjM1MTAxNjk1.m2q2MOZR3vArhqg1nC4-i2CEaVPlcPNcbic3KyTGj-cg.BBprGk0SqCmOMngKaT1CaaR_IBTJ8t-4LrOu_Nn2prAg.JPEG.p197273/88aad6.jpg?type=w800",
+            tags = listOf("태그", "태그", "태그")
+        )
     }
 }
