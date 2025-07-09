bc6f83acab3a6053b1edd44b5cfd0a0a32514f1a
Jyunee54 <jyunee54@gmail.com>
2025-07-10 05:51:04 +0900

    [feat] 날짜 입력 포맷팅

---
+++ app/src/main/java/org/sopt/certi/presentation/ui/resume/component/ResumeDateInputRow.kt
@@ -0,0 +1,72 @@
+package org.sopt.certi.presentation.ui.resume.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.certi.R
+import org.sopt.certi.ui.theme.CERTITheme
+import org.sopt.certi.ui.theme.CertiTheme
+
+@Composable
+fun ResumeDateInputRow(
+    startDate: MutableState<TextFieldValue>,
+    endDate: MutableState<TextFieldValue>,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ResumeDateTextField(
+            value = startDate.value,
+            onValueChange = { startDate.value = it }
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = stringResource(R.string.resume_date_input_text_1),
+            style = CertiTheme.typography.caption.semibold_14,
+            color = CertiTheme.colors.gray600
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        ResumeDateTextField(
+            value = endDate.value,
+            onValueChange = { endDate.value = it }
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = stringResource(R.string.resume_date_input_text_2),
+            style = CertiTheme.typography.caption.semibold_14,
+            color = CertiTheme.colors.gray600
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ResumeDateInputRowPreview() {
+    val startDate = remember { mutableStateOf(TextFieldValue("")) }
+    val endDate = remember { mutableStateOf(TextFieldValue("")) }
+
+    CERTITheme {
+        ResumeDateInputRow(
+            startDate = startDate,
+            endDate = endDate,
+            modifier = Modifier.padding(30.dp)
+        )
+    }
+}
--- app/src/main/java/org/sopt/certi/presentation/ui/resume/component/ResumeDateTextField.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/resume/component/ResumeDateTextField.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,14 +46,15 @@ fun ResumeDateTextField(
         },
         modifier = modifier
             .clip(RoundedCornerShape(12.dp))
-            .background(CertiTheme.colors.white)
             .border(
                 width = 1.dp,
                 color = CertiTheme.colors.gray100,
                 shape = RoundedCornerShape(12.dp)
-            ),
+            )
+            .background(CertiTheme.colors.white),
         keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Number
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Done
         ),
         decorationBox = { innerTextField ->
             Box(
@@ -86,15 +87,11 @@ private fun formatDateInput(input: String): String {
 @Composable
 fun ResumeDateInputPreview() {
     val textState = remember { mutableStateOf(TextFieldValue("")) }
-
     CERTITheme {
-        Surface(
-            modifier = Modifier.padding(top = 30.dp)
-        ) {
-            ResumeDateTextField(
-                value = textState.value,
-                onValueChange = { textState.value = it }
-            )
-        }
+        ResumeDateTextField(
+            value = textState.value,
+            onValueChange = { textState.value = it },
+            modifier = Modifier.padding(20.dp)
+        )
     }
 }
