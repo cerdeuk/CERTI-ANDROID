8ed70fec30cb78b81a94675333fb6ef8c0f49464
mjth1s1s <navygrace8389@gmail.com>
2025-07-09 16:15:37 +0900

    [refactor] 코드리뷰 반영

--- app/src/main/java/org/sopt/certi/presentation/ui/home/component/FavoriteCertificationItem.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/home/component/FavoriteCertificationItem.kt
@@ -1,8 +1,5 @@
 package org.sopt.certi.presentation.ui.home.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,6 +23,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.certi.R
 import org.sopt.certi.core.util.heightForScreenPercentage
+import org.sopt.certi.core.util.noRippleClickable
+import org.sopt.certi.core.util.roundedBackgroundWithBorder
 import org.sopt.certi.core.util.widthForScreenPercentage
 import org.sopt.certi.domain.model.FavoriteCertificationData
 import org.sopt.certi.ui.theme.CertiTheme
@@ -35,23 +33,24 @@ import org.sopt.certi.ui.theme.CertiTheme
 fun FavoriteCertificationItem(
     favoriteCertificationData: FavoriteCertificationData,
     isFavorite: Boolean,
-    onFavoriteClicked: () -> Unit
+    onFavoriteClicked: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = Modifier
+        modifier = modifier
             .widthForScreenPercentage(0.444f)
             .heightForScreenPercentage(0.205f)
-            .background(CertiTheme.colors.white)
-            .border(
-                width = 1.dp,
-                color = CertiTheme.colors.gray100,
-                shape = RoundedCornerShape(12.dp)
+            .roundedBackgroundWithBorder(
+                cornerRadius = 12.dp,
+                backgroundColor = CertiTheme.colors.white,
+                borderColor = CertiTheme.colors.gray100,
+                borderWidth = 1.dp
             )
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 14.dp, vertical = 18.dp)
+                .padding(horizontal = 14.dp, vertical = 20.dp)
         ) {
             Row(
                 modifier = Modifier
@@ -71,15 +70,13 @@ fun FavoriteCertificationItem(
                     modifier = Modifier
                         .widthForScreenPercentage(0.066f)
                         .heightForScreenPercentage(0.03f)
-                        .clickable { onFavoriteClicked() }
+                        .noRippleClickable { onFavoriteClicked() }
                 )
             }
             Spacer(modifier = Modifier.height(8.dp))
 
             Text(
-                text = favoriteCertificationData.certificationName.let { name ->
-                    if (name.length > 6) name.chunked(6).joinToString("\n") else name
-                },
+                text = favoriteCertificationData.certificationName,
                 style = CertiTheme.typography.body.bold_18,
                 color = CertiTheme.colors.gray600,
                 maxLines = 2
@@ -88,7 +85,7 @@ fun FavoriteCertificationItem(
         Column(
             modifier = Modifier
                 .align(Alignment.BottomStart)
-                .padding(horizontal = 14.dp, vertical = 18.dp),
+                .padding(horizontal = 14.dp, vertical = 20.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             Row(
@@ -143,7 +140,7 @@ fun FavoriteCertificationItemPreview() {
     FavoriteCertificationItem(
         favoriteCertificationData = FavoriteCertificationData(
             certificationId = 1,
-            certificationName = "정보처리기사정보처리기사",
+            certificationName = "시각디자인산업기사",
             qualificationType = "국가기술자격",
             testType = "실기형",
             agencyName = "한국산업인력공단"
