6beb21c53a3be46ee88d6262c0f45e3114977b62
Jyunee54 <jyunee54@gmail.com>
2025-07-10 15:43:59 +0900

    [feat] 이력서 메인 뷰 구현

--- app/src/main/java/org/sopt/certi/core/component/topbar/CertiTopBar.kt
+++ app/src/main/java/org/sopt/certi/core/component/topbar/CertiTopBar.kt
@@ -21,7 +21,7 @@ fun CertiTopBar() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = screenWidthDp(22.dp), vertical = screenHeightDp(24.dp))
+            .padding(horizontal = screenWidthDp(20.dp), vertical = screenHeightDp(12.dp))
     ) {
         Image(
             painter = painterResource(R.drawable.img_logo_black),
--- app/src/main/java/org/sopt/certi/presentation/ui/resume/ResumeScreen.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/resume/ResumeScreen.kt
@@ -1,11 +1,20 @@
 package org.sopt.certi.presentation.ui.resume
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import org.sopt.certi.core.component.topbar.CertiTopBar
+import org.sopt.certi.core.util.screenHeightDp
+import org.sopt.certi.core.util.screenWidthDp
+import org.sopt.certi.presentation.ui.resume.component.ResumeProfile
 import org.sopt.certi.ui.theme.CERTITheme
 
 @Composable
@@ -13,20 +22,43 @@ fun ResumeRoute(
     padding: PaddingValues,
     viewModel: ResumeViewModel = hiltViewModel()
 ) {
-    ResumeScreen()
+    ResumeScreen(
+        modifier = Modifier.padding(padding),
+        jobCategory = listOf()
+    )
 }
 
 @Composable
 fun ResumeScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    jobCategory: List<String>
 ) {
-    Text(text = "Resume")
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) {
+        CertiTopBar()
+
+        LazyColumn {
+            item {
+                ResumeProfile(
+                    modifier = Modifier
+                        .padding(horizontal = screenWidthDp(20.dp))
+                        .padding(top = screenHeightDp(32.dp), bottom = screenHeightDp(36.dp)),
+                    category = jobCategory
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun PreviewResumeScreen() {
     CERTITheme {
-        ResumeScreen()
+        ResumeScreen(
+            jobCategory = listOf("IT/인터넷", "경영/사무", "경영/사무")
+        )
     }
 }
