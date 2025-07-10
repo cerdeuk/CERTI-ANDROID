ae4a1205ce8e359d8b0081da44b9c9fe740b50db
Jyunee54 <jyunee54@gmail.com>
2025-07-10 22:32:03 +0900

    [feat] 자격증 편집 아이템 컴포넌트 구현

---
+++ app/src/main/java/org/sopt/certi/presentation/ui/resume/component/ResumeMyCertiListItem.kt
@@ -0,0 +1,60 @@
+package org.sopt.certi.presentation.ui.resume.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.certi.R
+import org.sopt.certi.core.util.screenWidthDp
+import org.sopt.certi.domain.model.ResumeCertificationListData
+import org.sopt.certi.ui.theme.CERTITheme
+import org.sopt.certi.ui.theme.CertiTheme
+
+@Composable
+fun ResumeMyCertiListItem(
+    certification: ResumeCertificationListData,
+    modifier: Modifier = Modifier
+){
+    Row (
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        ResumeCertificationSmallCard(
+            certification = certification
+        )
+
+        Spacer(modifier = Modifier.width(screenWidthDp(12.dp)))
+
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_close_36),
+            contentDescription = null,
+            tint = CertiTheme.colors.gray500,
+            modifier = Modifier.size(screenWidthDp(36.dp))
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewResumeMyCertiListItem() {
+    CERTITheme {
+        ResumeMyCertiListItem(
+            certification = ResumeCertificationListData(
+                name = "GTQ 1급 (그래픽기술자격)",
+                year = 2025,
+                month = 7,
+                day = 3,
+                cardImageUrl = "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTBfMTgx/MDAxNTc4NjM1MTAxNjk1.m2q2MOZR3vArhqg1nC4-i2CEaVPlcPNcbic3KyTGj-cg.BBprGk0SqCmOMngKaT1CaaR_IBTJ8t-4LrOu_Nn2prAg.JPEG.p197273/88aad6.jpg?type=w800",
+                tags = listOf("태그", "태그", "태그")
+            )
+        )
+    }
+}
