279eb4b44af0d01565bf49598175a1b08d10b153
Jyunee54 <jyunee54@gmail.com>
2025-07-10 22:48:29 +0900

    [feat] 자격증 편집 뷰 구현

--- app/src/main/java/org/sopt/certi/presentation/ui/resume/ResumeMyCertScreen.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/resume/ResumeMyCertScreen.kt
@@ -1,14 +1,25 @@
 package org.sopt.certi.presentation.ui.resume
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import org.sopt.certi.R
+import org.sopt.certi.core.util.screenHeightDp
+import org.sopt.certi.core.util.screenWidthDp
+import org.sopt.certi.domain.model.ResumeCertificationListData
+import org.sopt.certi.presentation.ui.resume.component.ResumeMyCertiListItem
 import org.sopt.certi.ui.theme.CERTITheme
+import org.sopt.certi.ui.theme.CertiTheme
 
 @Composable
 fun ResumeMyCertRoute(
@@ -16,24 +27,80 @@ fun ResumeMyCertRoute(
     viewModel: ResumeViewModel = hiltViewModel()
 ) {
     ResumeMyCertScreen(
+        certifications = listOf(),
         modifier = Modifier.padding(padding)
     )
 }
 
 @Composable
 fun ResumeMyCertScreen(
+    certifications: List<ResumeCertificationListData>,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier.fillMaxSize()
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(
+            top = screenHeightDp(60.dp),
+            bottom = screenHeightDp(36.dp),
+            start = screenWidthDp(20.dp)
+        ),
+        verticalArrangement = Arrangement.spacedBy(screenHeightDp(36.dp))
     ) {
+        item {
+            Text(
+                text = stringResource(R.string.resume_section_certification_title),
+                style = CertiTheme.typography.subtitle.semibold_20,
+                color = CertiTheme.colors.gray600
+            )
+        }
+
+        items(certifications){certification ->
+            ResumeMyCertiListItem(
+                certification = certification
+            )
+        }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun PreviewResumeMyCertScreen() {
     CERTITheme {
-        ResumeMyCertScreen()
+        ResumeMyCertScreen(
+            certifications = listOf(
+                ResumeCertificationListData(
+                    name = "GTQ 1급 (그래픽기술자격)",
+                    year = 2025,
+                    month = 7,
+                    day = 3,
+                    cardImageUrl = "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTBfMTgx/MDAxNTc4NjM1MTAxNjk1.m2q2MOZR3vArhqg1nC4-i2CEaVPlcPNcbic3KyTGj-cg.BBprGk0SqCmOMngKaT1CaaR_IBTJ8t-4LrOu_Nn2prAg.JPEG.p197273/88aad6.jpg?type=w800",
+                    tags = listOf("태그", "태그", "태그")
+                ),
+                ResumeCertificationListData(
+                    name = "GTQ 1급 (그래픽기술자격)",
+                    year = 2025,
+                    month = 7,
+                    day = 3,
+                    cardImageUrl = "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTBfMTgx/MDAxNTc4NjM1MTAxNjk1.m2q2MOZR3vArhqg1nC4-i2CEaVPlcPNcbic3KyTGj-cg.BBprGk0SqCmOMngKaT1CaaR_IBTJ8t-4LrOu_Nn2prAg.JPEG.p197273/88aad6.jpg?type=w800",
+                    tags = listOf("태그", "태그", "태그")
+                ),
+                ResumeCertificationListData(
+                    name = "GTQ 1급 (그래픽기술자격)",
+                    year = 2025,
+                    month = 7,
+                    day = 3,
+                    cardImageUrl = "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTBfMTgx/MDAxNTc4NjM1MTAxNjk1.m2q2MOZR3vArhqg1nC4-i2CEaVPlcPNcbic3KyTGj-cg.BBprGk0SqCmOMngKaT1CaaR_IBTJ8t-4LrOu_Nn2prAg.JPEG.p197273/88aad6.jpg?type=w800",
+                    tags = listOf("태그", "태그", "태그")
+                ),
+                ResumeCertificationListData(
+                    name = "GTQ 1급 (그래픽기술자격)",
+                    year = 2025,
+                    month = 7,
+                    day = 3,
+                    cardImageUrl = "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTBfMTgx/MDAxNTc4NjM1MTAxNjk1.m2q2MOZR3vArhqg1nC4-i2CEaVPlcPNcbic3KyTGj-cg.BBprGk0SqCmOMngKaT1CaaR_IBTJ8t-4LrOu_Nn2prAg.JPEG.p197273/88aad6.jpg?type=w800",
+                    tags = listOf("태그", "태그", "태그")
+                )
+            )
+        )
     }
 }
