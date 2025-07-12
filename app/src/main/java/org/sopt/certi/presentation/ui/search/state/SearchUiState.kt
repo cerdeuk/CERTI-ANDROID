76570c2e05f6dd7ff3735d9c81b96876489aaff9
MinseoSONG <mins041600@naver.com>
2025-07-12 20:57:02 +0900

    [feat] 검색 뷰 - SearchUiState 구현

---
+++ app/src/main/java/org/sopt/certi/presentation/ui/search/state/SearchUiState.kt
@@ -0,0 +1,17 @@
+package org.sopt.certi.presentation.ui.search.state
+
+import org.sopt.certi.core.state.UiState
+import org.sopt.certi.domain.model.CertificationData
+
+data class SearchUiState(
+    val searchCertificationListLoadState: UiState<List<CertificationData>>,
+    val keyword : String,
+    val submittedKeyword: String
+){
+    val loadState: UiState<Unit>
+        get() = when(searchCertificationListLoadState){
+            is UiState.Success -> UiState.Success(Unit)
+            is UiState.Init -> UiState.Init
+            else -> UiState.Empty
+        }
+}
