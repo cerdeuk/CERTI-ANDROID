ba163726588577461b1d35214471267733bd819e
MinseoSONG <mins041600@naver.com>
2025-07-14 16:32:21 +0900

    [feat] baseResponse 및 서버 응답 파싱 처리

---
+++ app/src/main/java/org/sopt/certi/data/remote/dto/base/ApiResponse.kt
@@ -0,0 +1,16 @@
+package org.sopt.certi.data.remote.dto.base
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ApiResponse<T>(
+    @SerialName("status")
+    val status: Int,
+    @SerialName("code")
+    val code: String? = null,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: T
+)
