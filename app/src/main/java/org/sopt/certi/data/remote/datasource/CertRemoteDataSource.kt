1f12daf675f712848ba6d549b21d320472f446b6
MinseoSONG <mins041600@naver.com>
2025-07-16 16:59:37 +0900

    [feat] 자격증 검색 api - datasource 구현

--- app/src/main/java/org/sopt/certi/data/remote/datasource/CertRemoteDataSource.kt
+++ app/src/main/java/org/sopt/certi/data/remote/datasource/CertRemoteDataSource.kt
@@ -2,7 +2,9 @@ package org.sopt.certi.data.remote.datasource
 
 import org.sopt.certi.data.remote.dto.base.ApiResponse
 import org.sopt.certi.data.remote.dto.response.GetRecommendCertResponseDto
+import org.sopt.certi.data.remote.dto.response.SearchCertListResponseDto
 
 interface CertRemoteDataSource {
     suspend fun getRecommendCertList(): ApiResponse<GetRecommendCertResponseDto>
+    suspend fun searchCertList(keyword: String): ApiResponse<SearchCertListResponseDto>
 }
