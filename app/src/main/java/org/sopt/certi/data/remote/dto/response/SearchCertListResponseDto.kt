eac099bb9b00cae41eefe2436879f2c56901ff5a
MinseoSONG <mins041600@naver.com>
2025-07-16 16:57:57 +0900

    [feat] 자격증 검색 api - service 및 dto 구현

---
+++ app/src/main/java/org/sopt/certi/data/remote/dto/response/SearchCertListResponseDto.kt
@@ -0,0 +1,26 @@
+package org.sopt.certi.data.remote.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SearchCertListResponseDto(
+    @SerialName("certificationSimpleList")
+    val certificationSimpleList: List<CertificationSimple>
+)
+
+@Serializable
+data class CertificationSimple(
+    @SerialName("isFavorite")
+    val isFavorite: Boolean,
+    @SerialName("certificationId")
+    val certificationId: Long,
+    @SerialName("certificationName")
+    val certificationName: String,
+    @SerialName("certificationType")
+    val certificationType: String,
+    @SerialName("testType")
+    val testType: String,
+    @SerialName("tags")
+    val tags: List<String>
+)
--- app/src/main/java/org/sopt/certi/data/remote/service/CertService.kt
+++ app/src/main/java/org/sopt/certi/data/remote/service/CertService.kt
@@ -2,9 +2,16 @@ package org.sopt.certi.data.remote.service
 
 import org.sopt.certi.data.remote.dto.base.ApiResponse
 import org.sopt.certi.data.remote.dto.response.GetRecommendCertResponseDto
+import org.sopt.certi.data.remote.dto.response.SearchCertListResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CertService {
     @GET("api/v1/certification/recommend")
     suspend fun getRecommendCertList(): ApiResponse<GetRecommendCertResponseDto>
+
+    @GET("/api/v1/certification/search")
+    suspend fun searchCertList(
+        @Query("keyword") keyword: String
+    ): ApiResponse<SearchCertListResponseDto>
 }
