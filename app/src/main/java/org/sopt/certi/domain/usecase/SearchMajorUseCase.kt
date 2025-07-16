743b54cf73ba76d2b8fc85f131a157ac8490d300
MinseoSONG <mins041600@naver.com>
2025-07-16 16:05:00 +0900

    [chore] ktlint 적용

--- app/src/main/java/org/sopt/certi/domain/usecase/SearchMajorUseCase.kt
+++ app/src/main/java/org/sopt/certi/domain/usecase/SearchMajorUseCase.kt
@@ -2,9 +2,9 @@ package org.sopt.certi.domain.usecase
 
 import org.sopt.certi.domain.repository.AuthRepository
 
-class SearchMajorUseCase (
+class SearchMajorUseCase(
     private val authRepository: AuthRepository
-){
+) {
     suspend operator fun invoke(keyword: String): Result<List<String>> =
         authRepository.searchMajor(keyword = keyword)
 }
