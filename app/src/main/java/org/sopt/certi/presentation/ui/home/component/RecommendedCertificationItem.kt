f3e9cf5e0a404e2047d2b86e346e0f67c4b4a67a
kimjw2003 <kimjw@sidewalkplay.com>
2025-10-09 22:10:48 +0900

    [chore] ktlint

--- app/src/main/java/org/sopt/certi/presentation/ui/home/component/RecommendedCertificationItem.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/home/component/RecommendedCertificationItem.kt
@@ -1,6 +1,5 @@
 package org.sopt.certi.presentation.ui.home.component
 
-import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,7 +13,6 @@ import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
