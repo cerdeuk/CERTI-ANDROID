651c10fe53bd275bd8594ff3229ec1a1345a80c8
MinseoSONG <mins041600@naver.com>
2025-10-17 20:06:02 +0900

    [chore] 검색뷰 자격증 아이템 구조 수정

--- app/src/main/java/org/sopt/certi/presentation/ui/search/SearchScreen.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/search/SearchScreen.kt
@@ -9,9 +9,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -70,14 +74,14 @@ private fun SearchScreen(
 ) {
     Column(
         modifier = modifier
-            .padding(top = screenHeightDp(24.dp), start = screenWidthDp(20.dp), end = screenWidthDp(20.dp))
+            .padding(top = screenHeightDp(24.dp))
             .fillMaxSize()
     ) {
         CertiBasicTextField(
             value = searchUiState.keyword,
             onValueChange = onValueChange,
             onSearchClick = onSearchClick,
-            modifier = Modifier.padding(vertical = screenHeightDp(12.dp)),
+            modifier = Modifier.padding(vertical = screenHeightDp(12.dp), horizontal = screenWidthDp(20.dp)),
             maxLength = 20
         )
 
@@ -92,7 +96,9 @@ private fun SearchScreen(
             }
 
             is UiState.Empty -> {
-                Column {
+                Column(
+                    modifier = Modifier.padding(horizontal = screenWidthDp(20.dp))
+                ) {
                     val resultText = stringResource(id = R.string.search_result_title, 0)
                     val countString = "0"
                     val startIndex = resultText.indexOf(countString)
@@ -114,7 +120,8 @@ private fun SearchScreen(
                         },
                         style = CertiTheme.typography.caption.regular_14,
                         color = CertiTheme.colors.gray600,
-                        modifier = Modifier.padding(top = screenHeightDp(12.dp), bottom = screenHeightDp(16.dp))
+                        modifier = Modifier
+                            .padding(top = screenHeightDp(12.dp), bottom = screenHeightDp(16.dp))
                     )
                     Spacer(modifier = Modifier.padding(screenHeightDp(90.dp)))
 
@@ -210,7 +217,7 @@ private fun SearchSuccessScreen(
                 },
                 style = CertiTheme.typography.caption.regular_14,
                 color = CertiTheme.colors.gray600,
-                modifier = Modifier.padding(top = screenHeightDp(12.dp), bottom = screenHeightDp(16.dp))
+                modifier = Modifier.padding(top = screenHeightDp(12.dp), start = screenWidthDp(20.dp))
             )
         }
 
@@ -224,7 +231,14 @@ private fun SearchSuccessScreen(
                 onCertificationClick = {
                     navigateToCertDetail(item.certificationId)
                 },
-                modifier = Modifier.padding(bottom = screenHeightDp(12.dp))
+                modifier = Modifier
+                    .padding(top = screenHeightDp(16.dp), bottom = screenHeightDp(24.dp))
+                    .padding(horizontal = 20.dp)
+            )
+
+            HorizontalDivider(
+                color = CertiTheme.colors.gray100,
+                thickness = screenWidthDp(1.dp)
             )
         }
     }
@@ -233,6 +247,60 @@ private fun SearchSuccessScreen(
 @Preview(showBackground = true)
 @Composable
 private fun PreviewSearchScreen() {
+    CERTITheme {
+        var certificationList by remember {
+            mutableStateOf(
+                listOf(
+                    CertificationData(
+                        certificationId = 1,
+                        certificationName = "정보처리기사",
+                        tags = listOf("시각디자인", "컴퓨터공학", "경영"),
+                        isFavorite = true,
+                        testType = "실기형",
+                        agencyName = "국가기술자격",
+                        description = "자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다.자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다.자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다."
+                    ),
+                    CertificationData(
+                        certificationId = 2,
+                        certificationName = "GTQ 1급 (그래픽 기술 자격)",
+                        tags = listOf("시각디자인", "컴퓨터공학", "경영"),
+                        isFavorite = false,
+                        testType = "실기형",
+                        agencyName = "국가기술자격",
+                        description = "자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다.자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다.자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다."
+                    ),
+                    CertificationData(
+                        certificationId = 3,
+                        certificationName = "TOEIC 900+",
+                        tags = listOf("경영", "시각디자인", "컴퓨터공학"),
+                        isFavorite = true,
+                        testType = "실기형",
+                        agencyName = "국가기술자격",
+                        description = "자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다.자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다.자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다. 자격증 설명입니다."
+                    )
+                )
+            )
+        }
+        val uiState = SearchUiState(
+            keyword = "",
+            submittedKeyword = "",
+            searchCertificationListLoadState = UiState.Success(certificationList)
+        )
+
+        SearchScreen(
+            searchUiState = uiState,
+            onValueChange = {},
+            onSearchClick = {},
+            certificationList = (uiState.searchCertificationListLoadState as? UiState.Success)?.data.orEmpty().toImmutableList(),
+            onLikeClick = {},
+            navigateToCertDetail = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewEmptyScreen() {
     CERTITheme {
         SearchScreen(
             searchUiState = SearchUiState(
