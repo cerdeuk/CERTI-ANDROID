d5574909456c21c7784a47f3be0113bedd3277df
Jyunee54 <jyunee54@gmail.com>
2025-11-09 05:14:29 +0900

    Revert "[feat] 마이페이지 메인 화면 수정"
    This reverts commit 421b8c64

--- app/src/main/java/org/sopt/certi/presentation/ui/my/PersonalInfoScreen.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/my/PersonalInfoScreen.kt
@@ -122,6 +122,8 @@ fun PersonalInfoScreen(
         item {
             PersonalInfoDateInputField(
                 label = stringResource(R.string.personal_birthday_label),
+                placeholder = stringResource(R.string.personal_birthday_placeholder),
+                value = uiState.birth,
                 onValueChange = onBirthChange
             )
         }
