4158f02c5d5053ab5cfd40f363ee8ee5b76c43ac
Jyunee54 <jyunee54@gmail.com>
2025-12-22 04:29:25 +0900

    [feat] 스크롤 수정

--- app/src/main/java/org/sopt/certi/presentation/ui/editacademicinfo/EditUnivNameScreen.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/editacademicinfo/EditUnivNameScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -46,7 +48,8 @@ fun EditUnivNameScreen(
 ) {
     Column(
         modifier = modifier
-            .fillMaxWidth()
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(horizontal = screenWidthDp(20.dp))
     ) {
         EditInfoHeader(
