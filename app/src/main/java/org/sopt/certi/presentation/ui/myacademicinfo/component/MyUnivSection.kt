ee82a6d2cacf2902cacd19bc1d25e1d26c97023a
Jyunee54 <jyunee54@gmail.com>
2025-12-24 04:31:17 +0900

    [chore] 컴포넌트 위치 수정

--- app/src/main/java/org/sopt/certi/presentation/ui/myacademicinfo/component/MyUnivSection.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/myacademicinfo/component/MyUnivSection.kt
@@ -1,19 +1,15 @@
 package org.sopt.certi.presentation.ui.myacademicinfo.component
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.certi.R
-import org.sopt.certi.core.util.noRippleClickable
+import org.sopt.certi.core.component.section.ArrowMenuSection
 import org.sopt.certi.core.util.screenHeightDp
 import org.sopt.certi.ui.theme.CERTITheme
 import org.sopt.certi.ui.theme.CertiTheme
@@ -33,38 +29,14 @@ fun MyUnivSection(
             color = CertiTheme.colors.gray400,
             modifier = Modifier.padding(bottom = screenHeightDp(24.dp))
         )
-        MyUnivManageItem(
+        ArrowMenuSection(
             text = stringResource(R.string.my_school_info_modify),
             onClick = onUnivManageClick,
             modifier = Modifier.padding(bottom = screenHeightDp(16.dp))
         )
-        MyUnivManageItem(
+        ArrowMenuSection(
             text = stringResource(R.string.my_school_major_modify),
             onClick = onMajorManageClick
-        )
-    }
-}
-
-@Composable
-private fun MyUnivManageItem(
-    text: String,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier
-    ) {
-        Text(
-            text = text,
-            style = CertiTheme.typography.body.semibold_16,
-            color = CertiTheme.colors.black,
-            modifier = Modifier.weight(1f)
-        )
-        Icon(
-            imageVector = ImageVector.vectorResource(R.drawable.ic_arrowright_24),
-            contentDescription = null,
-            tint = CertiTheme.colors.black,
-            modifier = Modifier.noRippleClickable { onClick() }
         )
     }
 }
