21dc7fbc455eb39fcd29cfc94ae841c914841df2
Jyunee54 <jyunee54@gmail.com>
2026-01-10 04:03:19 +0900

    [feat] 취득완료 바텀시트 구현

--- app/src/main/java/org/sopt/certi/presentation/ui/mycertification/component/EditAcquiredTestInfoBottomSheet.kt
+++ app/src/main/java/org/sopt/certi/presentation/ui/mycertification/component/EditAcquiredTestInfoBottomSheet.kt
@@ -1,10 +1,14 @@
 package org.sopt.certi.presentation.ui.mycertification.component
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
@@ -18,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -26,7 +31,14 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import org.sopt.certi.R
 import org.sopt.certi.core.component.button.CertiBasicButton
+import org.sopt.certi.core.component.calendar.DatePickerCalendar
+import org.sopt.certi.core.component.textfield.DropdownTextField
+import org.sopt.certi.core.util.heightForScreenPercentage
+import org.sopt.certi.core.util.roundedBackgroundWithBorder
+import org.sopt.certi.core.util.screenHeightDp
 import org.sopt.certi.core.util.screenWidthDp
+import org.sopt.certi.core.util.toLocalDateOrMin
+import org.sopt.certi.core.util.widthForScreenPercentage
 import org.sopt.certi.domain.model.certification.CertificationData
 import org.sopt.certi.ui.theme.CERTITheme
 import org.sopt.certi.ui.theme.CertiTheme
@@ -43,32 +55,45 @@ fun RegisterAcquiredInfoBottomSheet(
 
     var dateText by remember { mutableStateOf("") }
     var scoreText by remember { mutableStateOf("") }
+    var showCalendar by remember { mutableStateOf(false) }
 
     ModalBottomSheet(
         onDismissRequest = {
             onDismissClick()
         },
+        shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp),
+        containerColor = CertiTheme.colors.white,
         sheetState = sheetState,
-        modifier = modifier
+        modifier = modifier,
+        dragHandle = {
+            Box(
+                modifier = Modifier
+                    .padding(top = screenHeightDp(20.dp))
+                    .widthForScreenPercentage(80.dp)
+                    .heightForScreenPercentage(5.dp)
+                    .roundedBackgroundWithBorder(12.dp, CertiTheme.colors.gray200)
+            )
+        }
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentHeight()
                 .padding(horizontal = screenWidthDp(20.dp))
         ) {
+            Spacer(modifier = Modifier.heightForScreenPercentage(44.dp))
             Text(
-                text = stringResource(R.string.edit_acquired_certification_bottomsheet_title),
+                text = stringResource(R.string.edit_acquired_bottomsheet_title),
                 style = CertiTheme.typography.body.bold_18,
                 color = CertiTheme.colors.gray600
             )
-
+            Spacer(modifier = Modifier.heightForScreenPercentage(8.dp))
             Text(
                 text = certificationData.certificationName,
                 style = CertiTheme.typography.caption.semibold_14,
                 color = CertiTheme.colors.gray400
             )
 
+            Spacer(modifier = Modifier.heightForScreenPercentage(32.dp))
             Row {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.ic_check_24),
@@ -76,12 +101,74 @@ fun RegisterAcquiredInfoBottomSheet(
                     tint = CertiTheme.colors.gray500
                 )
                 Text(
-                    text = stringResource(R.string.edit_acquired_date),
+                    text = stringResource(R.string.edit_acquired_bottomsheet_date),
                     style = CertiTheme.typography.body.semibold_16,
                     color = CertiTheme.colors.gray600
                 )
             }
+            Spacer(modifier = Modifier.heightForScreenPercentage(12.dp))
+            DropdownTextField(
+                value = dateText,
+                placeholder = stringResource(R.string.edit_acquired_bottomsheet_date_placeholder),
+                onClick = { showCalendar = !showCalendar }
+            )
 
+            Box {
+                Column {
+                    Spacer(modifier = Modifier.heightForScreenPercentage(24.dp))
+                    Row {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.ic_check_24),
+                            contentDescription = null,
+                            tint = CertiTheme.colors.gray500
+                        )
+                        Text(
+                            text = stringResource(R.string.edit_acquired_bottomsheet_score),
+                            style = CertiTheme.typography.body.semibold_16,
+                            color = CertiTheme.colors.gray600
+                        )
+                    }
+                    Spacer(modifier = Modifier.heightForScreenPercentage(12.dp))
+                    BasicTextField(
+                        value = scoreText,
+                        onValueChange = { scoreText = it },
+                        textStyle = CertiTheme.typography.caption.semibold_12.copy(
+                            color = CertiTheme.colors.black
+                        ),
+                        maxLines = 1,
+                        singleLine = true,
+                        decorationBox = { innerTextField ->
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clip(RoundedCornerShape(4.dp))
+                                    .border(1.dp, CertiTheme.colors.gray200, RoundedCornerShape(4.dp))
+                                    .padding(screenWidthDp(12.dp))
+                            ) {
+                                if (scoreText.isEmpty()) {
+                                    Text(
+                                        text = stringResource(R.string.edit_acquired_bottomsheet_score_placerholder),
+                                        style = CertiTheme.typography.caption.semibold_12,
+                                        color = CertiTheme.colors.gray300
+                                    )
+                                }
+                            }
+                        }
+                    )
+                    Spacer(modifier = Modifier.heightForScreenPercentage(72.dp))
+                }
+
+                if (showCalendar) {
+                    DatePickerCalendar(
+                        selectedDate = dateText.toLocalDateOrMin(),
+                        onDateSelected = { date ->
+                            dateText = date.toString().replace("-", ".")
+                            showCalendar = false
+                        },
+                        modifier = Modifier.padding(bottom = screenHeightDp(44.dp))
+                    )
+                }
+            }
 
             CertiBasicButton(
                 buttonText = stringResource(R.string.test_info_bottomsheet_confirm),
@@ -93,6 +180,7 @@ fun RegisterAcquiredInfoBottomSheet(
                 },
                 modifier = Modifier.fillMaxWidth()
             )
+            Spacer(modifier = Modifier.heightForScreenPercentage(24.dp))
         }
     }
 }
