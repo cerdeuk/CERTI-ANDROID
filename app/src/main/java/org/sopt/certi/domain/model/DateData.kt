f203291f643d6ebf6b922d4747e8b67e53eb430c
Jyunee54 <jyunee54@gmail.com>
2026-01-28 21:57:36 +0900

    [feat] 날짜 매핑 위치 수정

--- app/src/main/java/org/sopt/certi/domain/model/DateData.kt
+++ app/src/main/java/org/sopt/certi/domain/model/DateData.kt
@@ -5,10 +5,6 @@ data class DateData(
     val month: Int? = null,
     val day: Int? = null
 ) {
-    val yearText: String get() = year?.toString() ?: ""
-    val monthText: String get() = month?.let { "%02d".format(it) } ?: ""
-    val dayText: String get() = day?.let { "%02d".format(it) } ?: ""
-
     val isAllEmpty: Boolean get() = year == null && month == null && day == null
     val isComplete: Boolean get() = year != null && month != null && day != null
     val isValid: Boolean get() = isAllEmpty || isComplete
