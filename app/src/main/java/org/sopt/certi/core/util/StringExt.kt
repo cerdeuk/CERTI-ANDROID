0dc87950cccf106d3693456d6b5a7ac400a91c58
kimjw <kimjw111003@gmail.com>
2026-03-13 19:04:57 +0900

    [Chore] ktlint

--- app/src/main/java/org/sopt/certi/core/util/StringExt.kt
+++ app/src/main/java/org/sopt/certi/core/util/StringExt.kt
@@ -2,7 +2,6 @@ package org.sopt.certi.core.util
 
 import org.sopt.certi.domain.model.DateData
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.Locale
